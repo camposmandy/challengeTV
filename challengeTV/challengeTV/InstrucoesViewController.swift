2a723eaa9083a7862dfe62f40222a10985fcab3c
Analisapessoa <anaelisapessoaguiar@gmail.com>
2016-05-11 15:51:52 -0300

    Criando classes e colocando as imagens no projeto

---
+++ challengeTV/challengeTV/InstrucoesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InstrucoesViewController.swift
+//  challengeTV
+//
+//  Created by Ana Elisa Pessoa Aguiar on 11/05/16.
+//  Copyright © 2016 Amanda Campos. All rights reserved.
+//
+
+import UIKit
+
+class InstrucoesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
