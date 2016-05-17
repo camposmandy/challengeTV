7d58949c02186a86f6b3a6210f92261d55874355
Amanda Campos <Amanda@amanda.local>
2016-05-17 13:46:15 -0300

    view de categorias programada

---
+++ challengeTV/challengeTV/CategoriaCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CategoriaCollectionViewCell.swift
+//  challengeTV
+//
+//  Created by Amanda Campos on 16/05/16.
+//  Copyright © 2016 Amanda Campos. All rights reserved.
+//
+
+import UIKit
+
+class CategoriaCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var img: UIImageView!
+    
+}
--- challengeTV/challengeTV/CategoriasViewController.swift
+++ challengeTV/challengeTV/CategoriasViewController.swift
@@ -8,28 +8,64 @@
 
 import UIKit
 
-class CategoriasViewController: UIViewController {
+class CategoriasViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    var  categorias = ["abacaxi.png", "Frutas.png", "Frutas2.png", "pirulito", "donut"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Collection
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
     }
-    */
-
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categorias.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("celula", forIndexPath: indexPath) as! CategoriaCollectionViewCell
+        
+        cell.img.image = UIImage(named: categorias[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("selecionou")
+    }
+ 
+    func collectionView(collectionView: UICollectionView, didUpdateFocusInContext context: UICollectionViewFocusUpdateContext, withAnimationCoordinator coordinator:UIFocusAnimationCoordinator) {
+        
+        if let previousIndexPath = context.previouslyFocusedIndexPath,
+            let cell = collectionView.cellForItemAtIndexPath(previousIndexPath) {
+            cell.contentView.layer.borderWidth = 0.0
+            cell.contentView.layer.shadowRadius = 0.0
+            cell.contentView.layer.shadowOpacity = 0
+        }
+        
+        if let indexPath = context.nextFocusedIndexPath,
+            let cell = collectionView.cellForItemAtIndexPath(indexPath) {
+//            cell.contentView.layer.borderWidth = 3.0
+//            cell.contentView.layer.borderColor = UIColor.blackColor().CGColor
+            cell.contentView.layer.shadowColor = UIColor.blackColor().CGColor
+            cell.contentView.layer.shadowRadius = 10.0
+            cell.contentView.layer.shadowOpacity = 0.9
+            cell.contentView.layer.shadowOffset = CGSize(width: 20, height: 20)
+            collectionView.scrollToItemAtIndexPath(indexPath, atScrollPosition: [.CenteredHorizontally, .CenteredVertically], animated: true)
+        }
+    }
 }
