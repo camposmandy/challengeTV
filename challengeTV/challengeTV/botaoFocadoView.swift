e917b810b04a8b0d0d6c12dab971e85d4c1c0eed
Analisapessoa <anaelisapessoaguiar@gmail.com>
2016-05-20 14:34:47 -0300

    Classe do botão

---
+++ challengeTV/challengeTV/botaoFocadoView.swift
@@ -0,0 +1,34 @@
+//
+//  botaoFocadoView.swift
+//  challengeTV
+//
+//  Created by Ana Elisa Pessoa Aguiar on 20/05/16.
+//  Copyright © 2016 Amanda Campos. All rights reserved.
+//
+
+import UIKit
+
+class botaoFocadoView: UIButton {
+
+    override func canBecomeFocused() -> Bool {
+        return true
+    }
+    
+    override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
+        
+        if context.nextFocusedView == self {
+            UIView.animateWithDuration(0.1, animations: { () -> Void in
+                
+                context.nextFocusedView?.transform = CGAffineTransformMakeScale(1.20, 1.20)
+            })
+        }
+        
+        if context.previouslyFocusedView == self {
+            UIView.animateWithDuration(0.1, animations: { () -> Void in
+                
+                context.nextFocusedView?.transform = CGAffineTransformMakeScale(1.0, 1.0)
+            })
+        }
+    }
+
+}
