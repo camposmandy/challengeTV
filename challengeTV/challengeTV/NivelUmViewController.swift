ffcac803eab9c1838776a246902492163c2a2637
Amanda Campos <Amanda@amanda.local>
2016-05-20 17:46:50 -0300

    animações

--- challengeTV/challengeTV/NivelUmViewController.swift
+++ challengeTV/challengeTV/NivelUmViewController.swift
@@ -28,7 +28,7 @@ class NivelUmViewController: UIViewController {
         super.viewDidLoad()
         
         cartas = [carta1, carta2, carta3, carta4, carta5, carta6]
-        opcoesCarta = ["Panda.png", "Panda.png", "Panda.png", "Panda.png", "Panda.png", "cupcake.png"]
+        opcoesCarta = ["Panda.png", "cupcake.png", "cupcake.png", "Panda.png", "Panda.png", "cupcake.png"]
         retorno = managerJogo.embaralhar(opcoesCarta) //retorna um array de cartas embaralhadas.
         
         carta1.imageView?.adjustsImageWhenAncestorFocused = true //teste de focused
@@ -47,15 +47,18 @@ class NivelUmViewController: UIViewController {
         interacao()
         //adiciona o indice do botão que foi selecionado
         selecionados.append(self.imagem(sender as! UIButton).indice)
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 25), dispatch_get_main_queue()) {
+            sender.setBackgroundImage(UIImage(named: img), forState: .Normal)
+        }
         UIView.transitionWithView(sender as! UIView, duration: 0.5, options: .TransitionFlipFromRight, animations: {
             }) { (finished) in
-                
-                sender.setBackgroundImage(UIImage(named: img), forState: .Normal)
                 self.jogo(sender as! UIButton)
         }
     }
     
     func jogo(carta: UIButton){ // compara se as cartas soteadas são iguais
+
         if selecionados.count == 1 {
             for c in cartas {
                 if c == carta{
@@ -68,9 +71,11 @@ class NivelUmViewController: UIViewController {
             //compara as cartas pelo nome da foto
             if self.imagem(cartas[selecionados[0]]).foto == self.imagem(cartas[selecionados[1]]).foto{
                 //acertou
+                acerto()
                 print("uhul")
             } else {
                 //errou
+                NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(self.erro), userInfo: nil, repeats: false)
                 print("quase :(")
             }
         }
@@ -80,6 +85,20 @@ class NivelUmViewController: UIViewController {
         for c in cartas{
             c.userInteractionEnabled = status
         }
+    }
+    
+    func acerto(){
+        animacaoAcerto(cartas[selecionados[0]])
+        animacaoAcerto(cartas[selecionados[1]])
+        
+        selecionados.removeAll()
+    }
+    
+    func erro(){
+        animacaoErro(cartas[selecionados[0]])
+        animacaoErro(cartas[selecionados[1]])
+        status = true
+        selecionados.removeAll()
     }
 
     func imagem(sender: UIButton) -> (foto: String, indice: Int){ // retorna a imagem do botão selecionado
@@ -94,5 +113,27 @@ class NivelUmViewController: UIViewController {
             i+=1
         }
         return (indice, j)
+    }
+    
+    func animacaoAcerto(sender: UIButton){
+        UIView.beginAnimations("teste", context: nil)
+        UIView.setAnimationDuration(1.0)
+        sender.transform = CGAffineTransformMakeScale(1.8, 1.8)
+        sender.alpha = 1.0
+        UIView.commitAnimations()
+        
+        UIView.beginAnimations("teste", context: nil)
+        UIView.setAnimationDuration(1.0)
+        sender.transform = CGAffineTransformMakeScale(1, 1)
+        sender.alpha = 1.0
+        UIView.commitAnimations()
+    }
+    
+    func animacaoErro(sender: UIButton){
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 250), dispatch_get_main_queue()) {
+            sender.setBackgroundImage(UIImage(named: "Parte de tras"), forState: .Normal)
+        }
+        UIView.transitionWithView(sender, duration: 0.5, options: .TransitionFlipFromLeft, animations: {
+            }, completion: nil)
     }
 }
