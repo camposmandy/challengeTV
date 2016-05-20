250a25df6d4b358d785a4be701be56d69118e7ad
Amanda Campos <Amanda@amanda.local>
2016-05-20 13:43:30 -0300

    animação e verificando se as cartas escolhidas são iguais.

--- challengeTV/challengeTV/JogoViewController.swift
+++ challengeTV/challengeTV/JogoViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  JogoViewController.swift
 //  challengeTV
@@ -8,28 +9,18 @@
 
 import UIKit
 
-class JogoViewController: UIViewController {
+class JogoViewController: NSObject {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var cartas = [String]()
 
-        // Do any additional setup after loading the view.
+        //Tem a função de embaralhar as cartas do jogos para não estarem sempre na mesma posição.
+    func embaralhar(c: Array<String>) -> Array<String> {
+        var cartasEmbaralhadas: Array<String> = []
+        for i in c{
+                let randomico = Int( arc4random() % UInt32(c.count))
+                print(i)
+                cartasEmbaralhadas.append(c[randomico])
+        }
+        return cartasEmbaralhadas
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ challengeTV/challengeTV/NivelUmViewController.swift
@@ -0,0 +1,96 @@
+//
+//  NivelUmViewController.swift
+//  challengeTV
+//
+//  Created by Amanda Campos on 18/05/16.
+//  Copyright © 2016 Amanda Campos. All rights reserved.
+//
+
+import UIKit
+
+class NivelUmViewController: UIViewController {
+    
+    var managerJogo = JogoViewController()
+    var opcoesCarta = [String]() //imagens das cartas
+    var cartas = [UIButton]() //array dos botões
+    var retorno = [String]()
+    var status = Bool()
+    var selecionados = [Int]()
+    
+    @IBOutlet weak var carta1: UIButton!
+    @IBOutlet weak var carta2: UIButton!
+    @IBOutlet weak var carta3: UIButton!
+    @IBOutlet weak var carta4: UIButton!
+    @IBOutlet weak var carta5: UIButton!
+    @IBOutlet weak var carta6: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        cartas = [carta1, carta2, carta3, carta4, carta5, carta6]
+        opcoesCarta = ["Panda.png", "Panda.png", "Panda.png", "Panda.png", "Panda.png", "cupcake.png"]
+        retorno = managerJogo.embaralhar(opcoesCarta) //retorna um array de cartas embaralhadas.
+        
+        carta1.imageView?.adjustsImageWhenAncestorFocused = true //teste de focused
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func animacaoCarta(sender: AnyObject) { //animação dos botões quando selecionados.
+        status = false
+        //enquanto houver animação todas as cartas estão sem interação
+        interacao()
+        //adiciona o indice do botão que foi selecionado
+        selecionados.append(self.imagem(sender as! UIButton).indice)
+        UIView.transitionWithView(sender as! UIView, duration: 0.5, options: .TransitionFlipFromRight, animations: {
+            }) { (finished) in
+                sender.setBackgroundImage(UIImage(named: self.imagem(sender as! UIButton).foto), forState: .Normal)
+                self.jogo(sender as! UIButton)
+        }
+    }
+    
+    func jogo(carta: UIButton){ // compara se as cartas soteadas são iguais
+        if selecionados.count == 1 {
+            for c in cartas {
+                if c == carta{
+                    c.userInteractionEnabled = false
+                } else {
+                    c.userInteractionEnabled = true
+                }
+            }
+        } else if selecionados.count == 2 {
+            //compara as cartas pelo nome da foto
+            if self.imagem(cartas[selecionados[0]]).foto == self.imagem(cartas[selecionados[1]]).foto{
+                //acertou
+                print("uhul")
+            } else {
+                //errou
+                print("quase :(")
+            }
+        }
+    }
+    
+    func interacao(){
+        for c in cartas{
+            c.userInteractionEnabled = status
+        }
+    }
+
+    func imagem(sender: UIButton) -> (foto: String, indice: Int){ // retorna a imagem do botão selecionado
+        var indice = ""
+        var i = 0
+        var j = 0
+        for carta in cartas{
+            if sender == carta {
+                indice = self.retorno[self.cartas.indexOf(carta)!]
+                j = i
+            }
+            i+=1
+        }
+        return (indice, j)
+    }
+}
