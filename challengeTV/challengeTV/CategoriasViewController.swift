969a753726dc5dc90f948666044062040df2fa16
mmedeiro <31313132@mackezista.com.br>
2016-05-24 15:04:19 -0300

    Vamos aproveitar para commitar enquanto estão ok, categorias e instruções!

--- challengeTV/challengeTV/CategoriasViewController.swift
+++ challengeTV/challengeTV/CategoriasViewController.swift
@@ -1,33 +1,25 @@
-//
-//  CategoriasViewController.swift
-//  challengeTV
-//
-//  Created by Ana Elisa Pessoa Aguiar on 11/05/16.
-//  Copyright © 2016 Amanda Campos. All rights reserved.
-//
-
 import UIKit
 
 class CategoriasViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     var  categorias = ["CategoriaAnimais.png", "CategoriaFrutas.png", "CategoriaNumeros.png"]
-    
+    var categoria = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Collection
-
+    
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
@@ -45,9 +37,10 @@ class CategoriasViewController: UIViewController, UICollectionViewDelegate, UICo
     }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        print("selecionou")
+        let i = indexPath.row
+        categoria = categorias[i]
     }
- 
+    
     func collectionView(collectionView: UICollectionView, didUpdateFocusInContext context: UICollectionViewFocusUpdateContext, withAnimationCoordinator coordinator:UIFocusAnimationCoordinator) {
         
         if let previousIndexPath = context.previouslyFocusedIndexPath,
@@ -55,6 +48,12 @@ class CategoriasViewController: UIViewController, UICollectionViewDelegate, UICo
             cell.contentView.layer.borderWidth = 0.0
             cell.contentView.layer.shadowRadius = 0.0
             cell.contentView.layer.shadowOpacity = 0
+            
+            UIView.animateWithDuration(0.1, animations: { () -> Void in
+                
+                context.previouslyFocusedView?.transform = CGAffineTransformMakeScale(1.0, 1.0)
+            })
+            
         }
         
         if let indexPath = context.nextFocusedIndexPath,
@@ -68,6 +67,11 @@ class CategoriasViewController: UIViewController, UICollectionViewDelegate, UICo
             cell.contentView.layer.shadowOpacity = 1
             cell.contentView.layer.shadowOffset = CGSizeZero
             collectionView.scrollToItemAtIndexPath(indexPath, atScrollPosition: [.CenteredHorizontally, .CenteredVertically], animated: true)
+            
+            UIView.animateWithDuration(0.1, animations: { () -> Void in
+                
+                context.nextFocusedView?.transform = CGAffineTransformMakeScale(1.13, 1.13)
+            })
         }
     }
 }
